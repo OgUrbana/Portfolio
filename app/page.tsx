6ba1b0c10328823b5ffd04fdc496a95fe0f0933f
Matthew Gajo <matthew.gajo@gmail.com>
2023-09-08 19:59:31 -0400

    typewriter text animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Hello from '@/components/Home/Hello';
 export default function Home() {
   return (
     <section className='padding-layout'>
-      <Hello />
+      <section className='w-full lg:w-5/12'>
+        <Hello />
+        <h1>Professional Web Developer based in USA</h1>
+      </section>
     </section>
   );
 }
--- components/Home/Hello.tsx
+++ components/Home/Hello.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+'use client';
 
-const titles = ['Matthew', 'a Developer', 'a Student', 'a Programmer'];
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+
+const titles = [
+  'Matthew Gajo',
+  1000,
+  'a Creator',
+  1000,
+  'an Innovator',
+  1000,
+  'an Aspiring Developer',
+  1000,
+];
 
 const Hello = () => {
   return (
     <h1 className='text-base font-semibold tracking-widest text-primary'>
-      Hey! I am {titles[0]}
+      Hey! I am{' '}
+      <TypeAnimation
+        sequence={titles}
+        repeat={Infinity}
+        className='after:text-white-500'
+      />
     </h1>
   );
 };
